58a3e23ecb1673b85388339c3000ca8cb293b570
highshineme <highshineme@gmail.com>
2022-05-03 16:54:56 +0900

    first commit

---
+++ Syntax_and_Expressions/target/debug/Syntax_and_Expressions.d
@@ -0,0 +1,1 @@
+/Users/gyoungminchoi/Documents/GitHub/rust_tutorial_practice/Syntax_and_Expressions/target/debug/Syntax_and_Expressions: /Users/gyoungminchoi/Documents/GitHub/rust_tutorial_practice/Syntax_and_Expressions/src/main.rs
---
+++ Syntax_and_Expressions/target/debug/deps/Syntax_and_Expressions-267c15bb116bf418.d
@@ -0,0 +1,5 @@
+/Users/gyoungminchoi/Documents/GitHub/rust_tutorial_practice/Syntax_and_Expressions/target/debug/deps/Syntax_and_Expressions-267c15bb116bf418: src/main.rs
+
+/Users/gyoungminchoi/Documents/GitHub/rust_tutorial_practice/Syntax_and_Expressions/target/debug/deps/Syntax_and_Expressions-267c15bb116bf418.d: src/main.rs
+
+src/main.rs:
